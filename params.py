5ce4eea53ed519fd1323b79f75453daf189e4aad
Seiya Kumada <s-kumada@nifty.com>
2016-10-24 13:37:50 +0000

    ...

--- params.py
+++ params.py
@@ -2,7 +2,7 @@
 
 #PRETRAINDED_MODEL_PATH = "/home/ubuntu/results/seq2seq/experiment_01/model_test.pkl"
 #PRETRAINDED_OPTIMIZER_PATH = "/home/ubuntu/results/seq2seq/experiment_01/optimizer_test.pkl"
-EPOCHS = 3000
+EPOCHS = 1000 
 MODEL_PATH = "/home/ubuntu/results/seq2seq/experiment_01/model.pkl"
 OPTIMIZER_PATH = "/home/ubuntu/results/seq2seq/experiment_01/optimizer.pkl"
 LOG_FILE_PATH = "/home/ubuntu/results/seq2seq/experiment_01/log"
